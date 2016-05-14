fdd3eac4bf3681ce5b93bf6c09346e458df4b42a
Amritha Dilip <amritha.dilip@sap.com>
2016-05-15 00:33:40 +0530

    work in progress
    needs debuggin on for and if
    
    Change-Id: I62ab03661deb7ffd2e15c8a1059f0d1d7e05c737

--- src/app/not-so-grey/not-so-grey.component.ts
+++ src/app/not-so-grey/not-so-grey.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {PathResolver} from '../shared/helpers/utilities';
+import {ISwatch} from '../shared/services/color.service';
 import {ColorService} from '../shared/services/color.service';
 
 let view = 'not-so-grey';
@@ -10,12 +11,44 @@ let view = 'not-so-grey';
 })
 
 export class NotSoGreyComponent {
+
+    colors: ISwatch[];
     constructor(private _colorService: ColorService) {
-        _colorService.getThemesFromColor('FF4444')
-            .then((collection) => {
-                console.log(collection.totalCount);
-                console.log(_.first(collection.themes).swatches);
-            })
-            .catch(exception => { console.error(exception); });
+        // _colorService.getThemesFromColor('FF4444')
+        //     .then((collection) => {
+        //         console.log(collection.totalCount);
+        //         console.log(_.first(collection.themes).swatches);
+        //     })
+        //     .catch(exception => { console.error(exception); });
+        
+        
+        
+        this.colors = [
+            {
+                name: "",
+                mode: "",
+                values: [22, 2, 3],
+                hex: "#333333",
+                colorIndex: '2'
+            },
+            {
+                name: "",
+                mode: "",
+                values: [22, 2, 3],
+                hex: "#333332",
+                colorIndex: '2'
+            },
+            {
+                name: "",
+                mode: "",
+                values: [22, 2, 3],
+                hex: "#333312",
+                colorIndex: '2'
+            }
+        ];
+
+
+
+        console.log(this.colors);
     }
 }
