32f24cae1324cd8ce14ba91bb4b73c363d9d035f
Bhargav Krishna <bhargav.krishna@outlook.com>
2016-05-14 13:35:40 -0700

    it works!!

--- src/app/not-so-grey/not-so-grey.component.ts
+++ src/app/not-so-grey/not-so-grey.component.ts
@@ -1,7 +1,6 @@
 import {Component} from '@angular/core';
 import {PathResolver} from '../shared/helpers/utilities';
-import {ISwatch} from '../shared/services/color.service';
-import {ColorService} from '../shared/services/color.service';
+import {ColorService, ISwatch} from '../shared/services/color.service';
 
 let view = 'not-so-grey';
 @Component({
@@ -14,22 +13,13 @@ export class NotSoGreyComponent {
 
     colors: ISwatch[];
     constructor(private _colorService: ColorService) {
-        // _colorService.getThemesFromColor('FF4444')
-        //     .then((collection) => {
-        //         console.log(collection.totalCount);
-        //         console.log(_.first(collection.themes).swatches);
-        //     })
-        //     .catch(exception => { console.error(exception); });
-        
-        
-        
         this.colors = [
             {
                 name: "",
                 mode: "",
                 values: [22, 2, 3],
                 hex: "#333333",
-am                colorIndex: '2'
+                colorIndex: '2'
             },
             {
                 name: "",
@@ -46,8 +36,6 @@ am                colorIndex: '2'
                 colorIndex: '2'
             }
         ];
-
-
 
         console.log(this.colors);
     }
