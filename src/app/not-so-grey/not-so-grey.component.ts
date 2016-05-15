096ef164a2987e653b0e0fe9aaa4bba8f92eec23
Amritha Dilip <amritha.dilip@sap.com>
2016-05-16 01:18:08 +0530

    changes for slider and grid. Data binding needs to done
    Change-Id: I6b9caabdfde5b9c73681dba09655012925f86509

--- src/app/not-so-grey/not-so-grey.component.ts
+++ src/app/not-so-grey/not-so-grey.component.ts
@@ -46,7 +46,28 @@ export class NotSoGreyComponent {
                 }
             });
         });*/
-
+        // click events
+        /*document.body.addEventListener('click', copy, true);
+        // event handler
+        function copy(e) {
+            // find target element
+            var t = e.target,
+                c = t.dataset.copytarget,
+                inp = (c ? document.querySelector(c) : null);
+            // is element selectable?
+            if (inp && inp.select) {
+                // select text
+                inp.select();
+                try {
+                    // copy text
+                    document.execCommand('copy');
+                    inp.blur();
+                }
+                catch (err) {
+                    alert('please press Ctrl/Cmd+C to copy');
+                }
+            }
+        }*/
     }
 
     getNumberOfVisibleItems($list): number {
