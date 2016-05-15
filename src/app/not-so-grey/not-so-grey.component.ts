b3f58f7f2e530739b2fcd79eedab2303c7fc9009
Amritha Dilip <amritha.dilip@sap.com>
2016-05-15 23:06:49 +0530

    Added different fragments.
    Change-Id: I1fa5dacfdbf4a7f3c66c02fecc768a37803d00c8

--- src/app/not-so-grey/not-so-grey.component.ts
+++ src/app/not-so-grey/not-so-grey.component.ts
@@ -1,6 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef} from '@angular/core';
 import {PathResolver} from '../shared/helpers/utilities';
-import {ColorService, ISwatch} from '../shared/services/color.service';
+import {ColorService} from '../shared/services/color.service';
 
 let view = 'not-so-grey';
 @Component({
@@ -11,32 +11,53 @@ let view = 'not-so-grey';
 
 export class NotSoGreyComponent {
 
-    colors: ISwatch[];
-    constructor(private _colorService: ColorService) {
-        this.colors = [
-            {
-                name: "",
-                mode: "",
-                values: [22, 2, 3],
-                hex: "#333333",
-                colorIndex: '2'
-            },
-            {
-                name: "",
-                mode: "",
-                values: [22, 2, 3],
-                hex: "#333332",
-                colorIndex: '2'
-            },
-            {
-                name: "",
-                mode: "",
-                values: [22, 2, 3],
-                hex: "#333312",
-                colorIndex: '2'
-            }
-        ];
 
-        console.log(this.colors);
+    private container;
+    constructor(
+        private _elementRef: ElementRef,
+        private _colorService: ColorService) {
+        /*_colorService.getThemesFromColor('FF4444')
+            .then((collection) => {
+                console.log(collection.totalCount);
+                console.log(_.first(collection.themes).swatches);
+            })
+            .catch(exception => { console.error(exception); });*/
+
+
+
+        this.container = _elementRef.nativeElement as HTMLElement;
+        this.container.querySelector('.color-list');
+
+    }
+
+    ngOnInit() {
+        var $colorList = this.container.querySelector('.color-list');
+        var noOfVisibleItems = this.getNumberOfVisibleItems($colorList);
+        /*$colorList.on("scroll", function () {
+            var nItem = 0;
+            $colorList.find('li').each(function (iIndex, $listItem) {
+                var $this = $(this);
+                var positionTop = $this.position().top;
+                var height = $this.height();
+                if (positionTop + $this.height() > 0 && positionTop < $colorList.height()) {
+                    nItem++;
+                    var fScaleValue = (nItem / noOfVisibleItems) * 9 * 16;
+                    $this.height(height + fScaleValue);
+                }
+            });
+        });*/
+
+    }
+
+    getNumberOfVisibleItems($list): number {
+        var noOfVisibleItems = 0;
+        $list.find('li').each(function (n) {
+            var $this = $(this);
+            console.log(n);
+            if ($this.position().top + $this.height() > 0 && $this.position().top < $list.height()) {
+                noOfVisibleItems++;
+            }
+        });
+        return noOfVisibleItems;
     }
 }
