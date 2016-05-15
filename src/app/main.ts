6d02b72d67cf97ef3fafb7819e5a2aaa90e2ffe6
Bhargav Krishna <bhargav.krishna@outlook.com>
2016-05-15 16:16:44 -0700

    swtiching to hashlocationstrategy

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,8 +1,10 @@
+import {provide} from '@angular/core';
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {ROUTER_PROVIDERS} from '@angular/router-deprecated';
 import {HTTP_PROVIDERS} from '@angular/http';
 import {NotSoGreyComponent} from './not-so-grey/not-so-grey.component';
 import {ImageHelper} from './shared/helpers/image.helper';
 import {DeviceHelper} from './shared/helpers/device.helper';
+import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 
-bootstrap(NotSoGreyComponent, [HTTP_PROVIDERS, ROUTER_PROVIDERS, ImageHelper, DeviceHelper]);
+bootstrap(NotSoGreyComponent, [HTTP_PROVIDERS, ROUTER_PROVIDERS, ImageHelper, DeviceHelper, provide(LocationStrategy, { useClass: HashLocationStrategy })]);
